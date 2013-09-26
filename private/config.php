136e3f043036e782e5a07d5d4bb2a1e520150845
Artur Czapla <arturczapla94@gmail.com>
2013-09-26 11:43:28 +0200

    zmiana schematu nazw stron

--- private/config.php
+++ private/config.php
@@ -2,9 +2,10 @@
 
 class Config {
     
-    public static $menu = array(array("PHP01 Zmienne","zmienne",true),
-                                array("PHP02 Funkcje","funkcje",true),
-                                array("JS 01 tabelka css","skrypty01",true));
+    public static $menu = array(array("PHP01 Zmienne","php01zmienne",true),
+                                array("PHP02 Funkcje","php02funkcje",true),
+                                array("PHP03 Tablice","php03tablice",true),
+                                array("JS 01 tabelka css","js01skrypty",true));
     
 }
 
---
+++ private/sites/php03tablice.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html lang="pl">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="Description" content="Strona testowa. Przykładowy opis strony" />
+	<meta name="Keywords" content="Przykładowa, testowa,strona,przykładowe,słowa, kluczowe" />
+	<title>PHP 03 Tablice</title>
+	<link rel="stylesheet" href="public/style/style.css" type="text/css" />
+</head>
+<body>
+<div id="wrapper">
+<?php
+	include('private/html/header.php');
+?>
+	<div id="container2">
+		<div id="container">
+			<?php 
+			 include('private/html/menu.php');
+			?>
+			<div id=contents>
+<?php
+    
+    
+    
+
+?>
+			</div>
+		</div>
+	</div>
+	<?php 
+	 include('private/html/footer.php');
+	?>
+	
+</div>
+</body>
+</html>

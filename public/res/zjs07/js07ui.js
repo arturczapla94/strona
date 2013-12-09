fe5f95fe8c6908205a6eed5df26af3b07da33686
klasa4 <klasa4@192.168.10.14>
2013-12-09 14:39:40 +0100

    JS JQuery UI

--- public/res/zjs07/js07ui.js
+++ public/res/zjs07/js07ui.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    $('#dr-block').draggable();
+    $('#dr-block').draggable({containment: "parent"});
     
-    $('#resizable-block').resizable({minHeight: 28,minWidth: 110});
+    $('#resizable-block').resizable({minHeight: 28,minWidth: 110,containment: "parent"});
     $('#selectable').selectable();
     
     $('#sortable').sortable();

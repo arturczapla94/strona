739355358693f9c6e84958a8bb062fe1cc6bc36d
Artur Czapla <arturczapla94@gmail.com>
2014-03-27 09:17:51 +0100

    wyswietlanie z bazy

--- private/sites/php12baza.php
+++ private/sites/php12baza.php
@@ -7,7 +7,55 @@
 $parametry = array();
 
 
-
+$mysqli = new mysqli(\Config\Config::$dbhost, \Config\Config::$dbuser, \Config\Config::$dbpass, \Config\Config::$dbname);
+    if ($mysqli->connect_errno)
+    {
+        $parametry['res'] = "error";
+        $parametry['msg']= "Błąd połączenia MySQL [" . $mysqli->connect_errno . "]: " . $mysqli->connect_error;
+    }
+    else
+    {
+        $mysqli->query("SET NAMES 'utf8'");
+        $t1 = "`".$mysqli->escape_string(\Config\Config::$dbprefix) ."cars`";
+        $strona = 1;
+        $ile = 50;
+        if(!empty($_GET['p']) && is_numeric($_GET['p']))
+            $strona = $_GET['p'];
+        $od=($strona-1)*$ile;
+        
+        $query = "SELECT COUNT(*) FROM $t1;";
+        $res = $mysqli->query($query);
+        if($res === false)
+        {
+            $parametry['res'] = "error";
+            $parametry['msg']= "Błąd zapytania";
+        } 
+        else 
+        {
+            $row = mysqli_fetch_row($res );
+            $parametry['num_rows']= $row[0];
+            $stron = floor($row[0]/$ile);
+        }
+        
+        $query = "SELECT * FROM $t1 LIMIT $od,$ile;";
+        $res = $mysqli->query($query);
+        if($res === false)
+        {
+            $parametry['res'] = "error";
+            $parametry['msg']= "Błąd zapytania";
+        } 
+        else 
+        {
+            $parametry['res'] = "ok";
+            while($column = mysqli_fetch_field($res))
+            {
+                $parametry['fields'][] = $column->name;
+            }
+            while ($row = mysqli_fetch_row($res )) {
+                $parametry['rows'][]= $row;
+            }
+        }
+    }
 
 
 //////////////////////////////////////
@@ -32,26 +80,37 @@ class CView extends inc\ViewBasic {
     {
         ?>
     
-        <article class="news-block block-block">
+        <article class=" block-block">
     <header class="block-top">
         
     </header>
     <div class="block-contents">
         
-        <div class="news-in2">
-            <div class="news-tags">
+       
+        <?php
+        /*print_r($this->dane['fields']);
+        print_r($this->dane['rows']);*/
 
-            </div> 
-            <section class="news-content">
-               <?php
-               
-               echo 'twój nr ip: '.$_SERVER['REMOTE_ADDR'].'<br>';
-               echo 'nazwa serwera: '.$_SERVER['SERVER_NAME'].'<br>';
-               echo 'skrypt: '.$_SERVER['PHP_SELF'].'<br>';
-               
-               ?>
-            </section>
-        </div>
+        echo "<table>";
+        echo "<tr>";
+        foreach ($this->dane['fields'] as $field)
+        {
+            echo "<th>$field</th>";
+        }
+        echo "</tr>";
+        foreach ($this->dane['rows'] as $row)
+        {
+            echo "<tr>";
+            foreach ($row as $field)
+            {
+                echo "<td>$field</td>";
+            }
+            echo "</tr>\n";
+        }
+        echo "</table>";
+        echo "r: ".$this->dane['num_rows'];
+        ?>
+
     </div>
 </article>
 <?php
